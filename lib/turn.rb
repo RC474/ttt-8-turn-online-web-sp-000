c4c162b7b7a93b72acc4f6e4a8da164c261a0754
Ramesh Chuwan <chhetrirahul2015@gmail.com>
2020-11-26 19:14:55 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,30 @@
+def display_board(
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
+  puts 'display board'
+#board output
+puts " #{board[0]} | #{board[1]} | #{board[2]} "
+puts "-----------"
+puts " #{board[3]} | #{board[4]} | #{board[5]} "
+puts "-----------"
+puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+
+def input_to_index(input)
+  user_input = input.to_i 
+  return user_input - 1
+end
+
+def valid_move?(board, index)
+  if index.between?(0, 8) && board[index] == " " || board[index] == "" || board[index] == nil 
+    return true
+  else
+    return false 
+  end
+end
+
+def move(board, index, token)
+  
+end
+  
+
